921cf8c02c031528cf774a2fd7a3fe0db33f0d77
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-11 14:58:00 +0500

    Refactors send mesages function

--- app/services/producer.service.js
+++ app/services/producer.service.js
@@ -2,19 +2,16 @@ const amqplib = require("amqplib/callback_api");
 const connection = require("./connection.service");
 
 const queue = "tasks";
-let taskCounter = 0;
 
 exports.sendMessages = () => {
-  amqplib.connect(process.env.RABBITMQ_SERVER, (error, connection) => {
-    connection.createChannel((err, channel1) => {
-      channel1.assertQueue();
+  let taskCounter = 0;
 
-      setInterval(() => {
-        taskCounter += 1;
-        channel1.sendToQueue(queue, Buffer.from(`Message ${taskCounter}`));
-        console.log(`Message ${taskCounter} sent`);
-      });
-    });
+  connection(queue, (error, channel) => {
+    setInterval(() => {
+      taskCounter += 1;
+      channel.sendToQueue(queue, Buffer.from(`Message ${taskCounter}`));
+      console.log(`Message ${taskCounter} sent`);
+    }, 1000);
   });
 };
 
